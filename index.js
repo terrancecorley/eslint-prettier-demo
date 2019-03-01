ecbac616008a69e090f1068a3bc36698f41d6172
Terrance Corley <tcorley@stepstoneglobal.com>
2019-03-01 09:48:05 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const someString    = "this is a test"
+
+let someVar = {destructuredValues}
+
+let obj2 = {
+someKey: "someString"
+}
+
+function testFunc() {
+let test = 'test'
+    let arr = [
+        'item2',
+        "item3"
+    ]
+}
